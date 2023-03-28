76b8f13f8b6250be18c08c979d6d5e9e68173bff
KJ <subodhsjagtap19@gmail.com>
2023-03-28 15:09:41 +0530

    props and dynamic expense

--- src/componets/ExpenseItem.js
+++ src/componets/ExpenseItem.js
@@ -1,9 +1,16 @@
-function ExpenseItem() {
-    return ( <div><h2>Expense Item</h2>
-            <h3>Food Rs 10</h3>
-            <h3>Petrol Rs 100</h3>
-            <h3>Movies Rs 200</h3> 
-        </div>)
+import './ExpenseItem.css';
+function ExpenseItem(props) {
+
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <h2>{props.location}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
