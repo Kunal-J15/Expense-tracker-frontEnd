e72f47fa0d086e6b7d528c2c121f8e48e345260a
KJ <subodhsjagtap19@gmail.com>
2023-03-28 18:56:39 +0530

    delete through DOM manipulation

--- src/componets/Expenses/ExpenseItem.js
+++ src/componets/Expenses/ExpenseItem.js
@@ -3,11 +3,15 @@ import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 import Card from '../UI/Card'
 function ExpenseItem(props) {
-
+  const handleClick = (evnt)=>{
+     const expense = document.getElementById(props.id);
+     expense.remove();
+  }
   return (
-    <Card className="expense-item">
+    <Card className="expense-item" id={props.id}>
      <ExpenseDate date = {props.date}></ExpenseDate>
      <ExpenseDetails amount={props.amount}  location={props.location}   title={props.title} />
+     <button onClick={handleClick}>Delete</button>
     </Card>
   );
 }
--- src/componets/UI/Card.js
+++ src/componets/UI/Card.js
@@ -2,7 +2,7 @@ import "./Card";
 export default function Card(props) {
     const classes = "Card "+props.className;
     return (
-        <div className={classes}>
+        <div className={classes} id={props.id}>
             {props.children}
         </div>
     )
